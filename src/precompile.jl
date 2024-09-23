1ee56298184a8652051ad7cac1155a33aca7a959
Adriano Meligrana <68152031+Tortar@users.noreply.github.com>
2024-09-23 13:17:38 +0200

    Fix formatting

--- src/precompile.jl
+++ src/precompile.jl
@@ -8,15 +8,15 @@ using PrecompileTools
 		  end)
         )
         _hybrid(:(struct B{X} <: AbstractB{X}
-					  x::X
-					  y::Int
-					  const z
-					  B{X}(x, y) where X = new{X}(x, y, 1)
-					  B{X}(x, y, z) where X = new{X}(x, y, z)
-					  function B(x::X, y, z) where X
-					      return new{X}(x, y, z)
-					  end
-				  end)
+		      x::X
+		      y::Int
+		      const z
+		      B{X}(x, y) where X = new{X}(x, y, 1)
+		      B{X}(x, y, z) where X = new{X}(x, y, z)
+		      function B(x::X, y, z) where X
+		          return new{X}(x, y, z)
+		      end
+		  end)
         )
     end
 end
